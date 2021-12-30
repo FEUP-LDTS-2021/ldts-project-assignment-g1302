645aeceb6924537d9b7be74a76a717ab410432f6
Gonçalo Marques <goncalomarques.abacate@gmail.com>
2021-12-30 17:20:21 +0000

    added scatter test

--- src/test/java/pt/up/fe/ldts/model/EmployeeTest.java
+++ src/test/java/pt/up/fe/ldts/model/EmployeeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import pt.up.fe.ldts.model.employeeAI.EmployeeAI;
+import pt.up.fe.ldts.model.employeeAI.ScatterAI;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -40,5 +41,40 @@ public class EmployeeTest {
         Mockito.verify(testAI, Mockito.times(1)).chooseTarget(Mockito.any());
         Assertions.assertEquals(1, test.get());
         Assertions.assertEquals(Entity.Direction.DOWN, employee.getDirection());
+    }
+
+
+    @Test
+    public void ScatterTest(){
+
+        EmployeeAI testAI = new ScatterAI();
+
+        Employee toni, ze_castro, baltazar, mariana;
+
+        toni = new Employee(10, 10, testAI, Employee.EmployeeName.TONI);
+
+        ze_castro = new Employee(10,10,testAI, Employee.EmployeeName.ZE_CASTRO);
+
+        baltazar = new Employee(10,10, testAI, Employee.EmployeeName.BALTAZAR);
+
+        mariana = new Employee(10,10, testAI, Employee.EmployeeName.MARIANA);
+
+        Assertions.assertEquals(10, toni.getX());
+        Assertions.assertEquals(10, toni.getY());
+        Assertions.assertEquals(Entity.Direction.LEFT, toni.getDirection());
+        
+        toni.changeDirection();
+        ze_castro.changeDirection();
+        baltazar.changeDirection();
+        mariana.changeDirection();
+
+        Assertions.assertEquals(Entity.Direction.LEFT, toni.getDirection());
+
+        Assertions.assertEquals(Entity.Direction.DOWN, ze_castro.getDirection());
+
+        Assertions.assertEquals(Entity.Direction.UP, baltazar.getDirection());
+
+        Assertions.assertEquals(Entity.Direction.DOWN, mariana.getDirection());
+
     }
 }
